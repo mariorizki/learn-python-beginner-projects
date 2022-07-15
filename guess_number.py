1cd50177614b8a07df2bf1d66a0a9ffc7680bd84
Mario Rizki <mariorizki07@gmail.com>
2022-07-15 21:47:23 +0700

    firs commit

---
+++ guess_number.py
@@ -0,0 +1,20 @@
+import random
+
+
+def guess_number():
+    random_number = random.randint(1, 100)
+
+    print('Guess number between 1 to 100')
+    guess = 0
+
+    while guess != random_number:
+        guess = int(input('input a number: '))
+        if guess < random_number:
+            print('Number too low, try again')
+        elif guess > random_number:
+            print('Number too high, try again!')
+
+    print('Congrats, you have guessed it right!')
+
+
+guess_number()
